664266ff10cb76223d77ed31c1d64fc2c76bfaea
orange-apples <1104888697@qq.com>
2020-05-30 17:28:53 +0800

    添加了查询用户地址的功能

--- src/main/java/com/galaxy/easybuy/controller/AccountController.java
+++ src/main/java/com/galaxy/easybuy/controller/AccountController.java
@@ -1,10 +1,14 @@
 package com.galaxy.easybuy.controller;
 
+import com.galaxy.easybuy.entity.Account;
+import com.galaxy.easybuy.entity.AccountAddress;
 import com.galaxy.easybuy.service.AccountService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 /**
  * (Account)表控制层
@@ -21,4 +25,11 @@ public class AccountController {
     @Resource
     private AccountService accountService;
 
+    @RequestMapping("/getAccountAddress")
+    public List<AccountAddress> getAccountAddress(HttpServletRequest request){
+        //TODO 根据用户id查询地址
+        //Account loginAccount = (Account) request.getSession().getAttribute("loginAccount");
+
+        return accountService.getAccountAddress(1);
+    }
 }
---
+++ src/main/java/com/galaxy/easybuy/entity/AccountAddress.java
@@ -0,0 +1,27 @@
+package com.galaxy.easybuy.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+/**
+ * (AccountAddress)实体类
+ *
+ * @author makejava
+ * @since 2020-05-30 16:40:33
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AccountAddress implements Serializable {
+    private static final long serialVersionUID = 256167821068167028L;
+    
+
+    private String address;
+    
+    private String remark;
+
+
+}
