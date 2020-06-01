4f61816c07db7324a7e115b6ffa5a148d2b6ec6e
orange-apples <1104888697@qq.com>
2020-06-01 15:45:04 +0800

    设置了允许跨域请求

---
+++ src/main/java/com/galaxy/easybuy/component/AccessInterceptor.java
@@ -0,0 +1,30 @@
+package com.galaxy.easybuy.component;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class AccessInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        response.addHeader("Access-Control-Allow-Origin", "*");
+        String localAddr = request.getRemoteAddr();
+        //response.addHeader("Access-Control-Allow-Origin","*");
+        System.out.println(localAddr+"-->"+new SimpleDateFormat("HH:mm:ss").format(new Date()) +"访问了你的主机！");
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+    }
+}
